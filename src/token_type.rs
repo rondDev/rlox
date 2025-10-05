86c4a5982d0cb522e4288a4265873c58504f6144
rondDev <contact@rond.cc>
2025-10-05 08:23:47 +0200

    feat: initial implementation of token type

---
+++ src/token_type.rs
@@ -0,0 +1,47 @@
+#[allow(non_camel_case_types, clippy::upper_case_acronyms)]
+#[derive(Debug)]
+pub enum TokenType {
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
+
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
+
+    EOF,
+}
